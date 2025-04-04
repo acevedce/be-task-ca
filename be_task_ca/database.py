ef54031b7ca6b94d91ca060e986e622d4a02482c
acevedce <ing.cesaracevedo@gmail.com>
2025-04-04 02:33:42 +0200

    FIX: change postgres port due to address in use

--- be_task_ca/database.py
+++ be_task_ca/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://postgres:example@localhost:5432/postgres")
+engine = create_engine("postgresql://postgres:example@localhost:5433/postgres")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
